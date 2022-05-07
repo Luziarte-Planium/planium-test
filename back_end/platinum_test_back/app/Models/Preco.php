eedde8e4a846d7a6c74f300d0e3649e4d44ac7c5
Alexsandron3 <alexsandro0602099@outlook.com>
2022-05-07 12:58:27 -0300

    Feature: Cria método para calcular preco por faixa de idade

--- back_end/platinum_test_back/app/Models/Preco.php
+++ back_end/platinum_test_back/app/Models/Preco.php
@@ -16,4 +16,27 @@ class Preco extends Model
         $precos = json_decode($arquivo);
         return $precos;
     }
+
+    public function listarPorId(int $codigo): array
+    {
+        $precos = $this->listar();
+
+        $preco = array_filter($precos, function ($preco) use ($codigo) {
+            return $preco->codigo === $codigo;
+        });
+        return $preco;
+    }
+
+    public function precoPorFaixaDeIdade(int $idade)
+    {
+        $precos = $this->listar();
+        if ($idade >= 17) {
+            $this->preco = $precos[0]->faixa1;
+        } elseif ($idade <= 18 && $idade >= 40) {
+            $this->preco = $precos[0]->faixa2;
+        } else {
+            $this->preco = $precos[0]->faixa3;
+        }
+        return $this->preco;
+    }
 }
