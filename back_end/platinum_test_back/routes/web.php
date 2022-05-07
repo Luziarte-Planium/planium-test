3a057fc9e60273edd9b7d3b81f3ed1644c6c8657
Alexsandron3 <alexsandro0602099@outlook.com>
2022-05-07 12:45:46 -0300

    Feature: Cria rota para cadastrar um novo beneficiário

--- back_end/platinum_test_back/routes/web.php
+++ back_end/platinum_test_back/routes/web.php
@@ -2,17 +2,11 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+$router->group(['prefix' => '/beneficiario'], function () use ($router) {
+    $router->post('/', 'BeneficiariosController@novoBeneficario');
 });
