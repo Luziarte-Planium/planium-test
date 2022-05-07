202e78b508693c31e741bdef465c5b96233895f9
Alexsandron3 <alexsandro0602099@outlook.com>
2022-05-07 12:56:54 -0300

    Feature: Cria model de beneficiário e método para cadastro

---
+++ back_end/platinum_test_back/app/Models/Beneficiario.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Beneficiario extends Model
+{
+    //
+    public string $nome;
+    public int $idade;
+    public int $beneficiarios;
+    public float $valorDoPlano;
+    public array $plano;
+
+    public function __construct($nome, $idade, $beneficiarios)
+    {
+        $this->nome = $nome;
+        $this->idade = $idade;
+        $this->beneficiarios = $beneficiarios;
+    }
+
+    public function cadastrar(float $valorDoPlano, array $plano): array
+    {
+        $this->plano = $plano;
+        $this->valorDoPlano = $valorDoPlano;
+        $propostas = json_decode(file_get_contents("../database/json/proposta.json"));
+        $novaProposta = array(
+            "codigo" => uniqid(),
+            "nome" => $this->nome,
+            "idade" => $this->idade,
+            "beneficiarios" => $this->beneficiarios,
+            "valorDoPlano" => $this->valorDoPlano,
+            "plano" => $this->plano
+        );
+
+        array_push($propostas, $novaProposta);
+        file_put_contents("../database/json/proposta.json", json_encode($propostas));
+        return $novaProposta;
+    }
+}
